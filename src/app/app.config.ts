6159aa44a323fefb69ee2a45e138498b11598c4c
JFredMC <desarrollador.its3@metgroupsas.com>
2025-10-04 13:51:43 -0500

    app interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routing';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './cores/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
