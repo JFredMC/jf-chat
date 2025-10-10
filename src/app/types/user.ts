2bcbfc124fb0d50a56735e8a29940eae76d71778
Jhon Fredy Maquilon Cordoba <83928959+JFredMC@users.noreply.github.com>
2025-10-10 00:49:48 -0500

    Actualizar user.ts

--- src/app/types/user.ts
+++ src/app/types/user.ts
@@ -3,7 +3,9 @@ import { IAudit } from "./audit";
 export interface IUser extends IAudit {
   id: number;
   username: string;
-  email: string;
-  name?: string;
-  avatar?: string;
+  fist_name: string;
+  last_name: string;
+  avatar_url?: string;
+  status: string;
+  is_active: boolean;
 }
